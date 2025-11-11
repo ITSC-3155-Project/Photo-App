997cf570dfb75dd491718ab0555444fc32b842e7
Alexander Wilson <awils235@charlotte.edu>
2025-11-11 16:32:12 -0500

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -2,14 +2,14 @@
 
 /**
  * Photo App Sprint 2 web server.
- * Uses MongoDB (project6) via Mongoose.
+ * Uses MongoDB (project6) via Mongoose v7+ (async/await, no callbacks).
  */
 
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 mongoose.set("strictQuery", false);
 
-const async = require("async");
+const asyncLib = require("async");
 const express = require("express");
 const app = express();
 
@@ -33,50 +33,55 @@ app.get("/", (req, res) => {
 /**
  * /test, /test/info, /test/counts
  */
-app.get("/test/:p1?", (req, res) => {
+app.get("/test/:p1?", async (req, res) => {
   const param = req.params.p1 || "info";
 
-  if (param === "info") {
-    SchemaInfo.find({}, (err, info) => {
-      if (err) {
-        res.status(500).send(JSON.stringify(err));
-        return;
-      }
+  try {
+    if (param === "info") {
+      const info = await SchemaInfo.find({}).exec();
       if (!info || info.length === 0) {
         res.status(500).send("Missing SchemaInfo");
         return;
       }
       res.status(200).send(info[0]);
-    });
-  } else if (param === "counts") {
-    const collections = [
-      { name: "user", collection: User },
-      { name: "photo", collection: Photo },
-      { name: "schemaInfo", collection: SchemaInfo },
-    ];
+    } else if (param === "counts") {
+      const collections = [
+        { name: "user", collection: User },
+        { name: "photo", collection: Photo },
+        { name: "schemaInfo", collection: SchemaInfo },
+      ];
 
-    async.each(
-      collections,
-      (col, done) => {
-        col.collection.countDocuments({}, (err, count) => {
-          col.count = count;
-          done(err);
-        });
-      },
-      (err) => {
-        if (err) {
-          res.status(500).send(JSON.stringify(err));
-          return;
+      // use asyncLib to keep structure close to original
+      asyncLib.each(
+        collections,
+        (col, done) => {
+          col.collection
+            .countDocuments({})
+            .exec()
+            .then((count) => {
+              col.count = count;
+              done();
+            })
+            .catch((err) => done(err));
+        },
+        (err) => {
+          if (err) {
+            res.status(500).send(JSON.stringify(err));
+            return;
+          }
+          const obj = {};
+          collections.forEach((c) => {
+            obj[c.name] = c.count;
+          });
+          res.status(200).send(obj);
         }
-        const obj = {};
-        collections.forEach((c) => {
-          obj[c.name] = c.count;
-        });
-        res.status(200).send(obj);
-      }
-    );
-  } else {
-    res.status(400).send("Bad param " + param);
+      );
+    } else {
+      res.status(400).send("Bad param " + param);
+    }
+  } catch (err) {
+    console.error("Error in /test:", err);
+    res.status(500).send(JSON.stringify(err));
   }
 });
 
@@ -84,27 +89,26 @@ app.get("/test/:p1?", (req, res) => {
  * GET /user/list
  * -> [{ _id, first_name, last_name }]
  */
-app.get("/user/list", (req, res) => {
-  User.find({}, "_id first_name last_name", (err, users) => {
-    if (err) {
-      res.status(500).send(JSON.stringify(err));
-      return;
-    }
-    res.status(200).send(
-      users.map((u) => ({
-        _id: u._id,
-        first_name: u.first_name,
-        last_name: u.last_name,
-      }))
-    );
-  });
+app.get("/user/list", async (req, res) => {
+  try {
+    const users = await User.find({}, "first_name last_name").exec();
+    const result = users.map((u) => ({
+      _id: u._id,
+      first_name: u.first_name,
+      last_name: u.last_name,
+    }));
+    res.status(200).send(result);
+  } catch (err) {
+    console.error("Error in /user/list:", err);
+    res.status(500).send(JSON.stringify(err));
+  }
 });
 
 /**
  * GET /user/:id
  * -> full user or 400 if invalid/not found
  */
-app.get("/user/:id", (req, res) => {
+app.get("/user/:id", async (req, res) => {
   const id = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -112,11 +116,8 @@ app.get("/user/:id", (req, res) => {
     return;
   }
 
-  User.findById(id, (err, user) => {
-    if (err) {
-      res.status(500).send(JSON.stringify(err));
-      return;
-    }
+  try {
+    const user = await User.findById(id).exec();
     if (!user) {
       res.status(400).send("Not found");
       return;
@@ -129,14 +130,17 @@ app.get("/user/:id", (req, res) => {
       description: user.description,
       occupation: user.occupation,
     });
-  });
+  } catch (err) {
+    console.error("Error in /user/:id:", err);
+    res.status(500).send(JSON.stringify(err));
+  }
 });
 
 /**
  * GET /photosOfUser/:id
  * -> photos for that user with comment.user populated
  */
-app.get("/photosOfUser/:id", (req, res) => {
+app.get("/photosOfUser/:id", async (req, res) => {
   const id = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -144,74 +148,63 @@ app.get("/photosOfUser/:id", (req, res) => {
     return;
   }
 
-  // Ensure user exists
-  User.findById(id, (err, user) => {
-    if (err) {
-      res.status(500).send(JSON.stringify(err));
-      return;
-    }
+  try {
+    // ensure the user exists
+    const user = await User.findById(id).exec();
     if (!user) {
       res.status(400).send("Not found");
       return;
     }
 
-    Photo.find({ user_id: id })
+    const photos = await Photo.find({ user_id: id }).lean().exec();
+
+    // collect all commenter user_ids
+    const commentUserIds = new Set();
+    photos.forEach((p) =>
+      (p.comments || []).forEach((c) => {
+        if (c.user_id) commentUserIds.add(String(c.user_id));
+      })
+    );
+
+    const usersForComments = await User.find(
+      { _id: { $in: Array.from(commentUserIds) } },
+      "first_name last_name"
+    )
       .lean()
-      .exec((err, photos) => {
-        if (err) {
-          res.status(500).send(JSON.stringify(err));
-          return;
-        }
+      .exec();
 
-        const commentUserIds = new Set();
-        photos.forEach((p) =>
-          (p.comments || []).forEach((c) => {
-            if (c.user_id) {
-              commentUserIds.add(String(c.user_id));
-            }
-          })
-        );
+    const userMap = {};
+    usersForComments.forEach((u) => {
+      userMap[String(u._id)] = {
+        _id: u._id,
+        first_name: u.first_name,
+        last_name: u.last_name,
+      };
+    });
 
-        User.find(
-          { _id: { $in: Array.from(commentUserIds) } },
-          "_id first_name last_name",
-          (err, usersForComments) => {
-            if (err) {
-              res.status(500).send(JSON.stringify(err));
-              return;
-            }
+    const responsePhotos = photos.map((p) => ({
+      _id: p._id,
+      file_name: p.file_name,
+      date_time: p.date_time,
+      user_id: p.user_id,
+      comments: (p.comments || []).map((c) => ({
+        _id: c._id,
+        comment: c.comment,
+        date_time: c.date_time,
+        user: userMap[String(c.user_id)],
+      })),
+    }));
 
-            const userMap = {};
-            usersForComments.forEach((u) => {
-              userMap[String(u._id)] = {
-                _id: u._id,
-                first_name: u.first_name,
-                last_name: u.last_name,
-              };
-            });
-
-            const responsePhotos = photos.map((p) => ({
-              _id: p._id,
-              file_name: p.file_name,
-              date_time: p.date_time,
-              user_id: p.user_id,
-              comments: (p.comments || []).map((c) => ({
-                _id: c._id,
-                comment: c.comment,
-                date_time: c.date_time,
-                user: userMap[String(c.user_id)],
-              })),
-            }));
-
-            if (responsePhotos.length === 0) {
-              res.status(400).send("Not found");
-            } else {
-              res.status(200).send(responsePhotos);
-            }
-          }
-        );
-      });
-  });
+    if (responsePhotos.length === 0) {
+      // For this assignment/tests: valid user id but no photos => 400.
+      res.status(400).send("Not found");
+    } else {
+      res.status(200).send(responsePhotos);
+    }
+  } catch (err) {
+    console.error("Error in /photosOfUser/:id:", err);
+    res.status(500).send(JSON.stringify(err));
+  }
 });
 
 const server = app.listen(3000, () => {
