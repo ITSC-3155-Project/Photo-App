a259534db734eeb37541371ff7b0982272cbf598
Sulaiman Mohamed <smoham20@charlotte.edu>
2025-11-11 17:01:15 -0500

    Update photo.js

--- photo.js
+++ photo.js
@@ -1,13 +1,15 @@
-"use strict";
+"use strict"; // Enforces strict mode for safer JavaScript (e.g., prevents using undeclared variables)
 
 const mongoose = require("mongoose");
 
+// Define the schema for comments on a photo
 const commentSchema = new mongoose.Schema({
   comment: String,
   date_time: { type: Date, default: Date.now },
   user_id: mongoose.Schema.Types.ObjectId,
 });
 
+// Define the schema for photos
 const photoSchema = new mongoose.Schema({
   file_name: String,
   date_time: { type: Date, default: Date.now },
@@ -17,4 +19,5 @@ const photoSchema = new mongoose.Schema({
 
 const Photo = mongoose.model("Photo", photoSchema);
 
+// Export the Photo model so it can be used in other files
 module.exports = Photo;
